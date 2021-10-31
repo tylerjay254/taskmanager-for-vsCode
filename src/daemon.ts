464942269c23b995e29992205fef29d5b57455e5
tylermwalo <tylerjaykenny@gmail.com>
2021-10-31 16:41:27 +0300

    daemon

---
+++ src/daemon.ts
@@ -0,0 +1,7 @@
+
+//the runs in the background , controls processes boots extension and posts notifications 
+//install dependancies as listed in package.json
+// handls user settings
+
+
+var reboot = setInterval( ()=> console.log ("rebooting sequence intitiated"), 30000 );
